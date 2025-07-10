d0dc166d028018e454164a6777fcf6234d5fed0f
Ty Busby <tybusby@gmail.com>
2025-07-10 14:01:52 -0400

    use src instead of dist

--- test/points.test.ts
+++ test/points.test.ts
@@ -1,5 +1,6 @@
 import { expect, test } from "vitest";
-import { NWSResponseError, Points } from "../dist";
+import { NWSResponseError } from "../src/error";
+import { Points } from "../src/points";
 
 test("get points should throw NWSResponseError", async () => {
     await expect(new Points(0, 0).get()).rejects.toThrow(NWSResponseError);
