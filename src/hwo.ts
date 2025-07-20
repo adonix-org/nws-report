802e89daae747792d8c94582578d98bdd97a1c87
Ty Busby <tybusby@gmail.com>
2025-07-20 02:45:23 -0400

    checkpoint

--- src/hwo.ts
+++ src/hwo.ts
@@ -16,28 +16,25 @@
 
 import { Product } from "./products";
 
-function parse(hwo: Product): void {
-    if (hwo.productCode !== "HWO") {
-        throw new Error(`Invalid product code ${hwo.productCode}`);
-    }
+function parse(product: Product): void {
     const delimiter = "\n\n";
-    let start = hwo.productText.indexOf(delimiter);
-    let end = hwo.productText.indexOf(delimiter, start + delimiter.length);
-    const heading = hwo.productText.slice(start + delimiter.length, end);
+    let start = product.productText.indexOf(delimiter);
+    let end = product.productText.indexOf(delimiter, start + delimiter.length);
+    const heading = product.productText.slice(start + delimiter.length, end);
     console.log(heading);
     console.log();
 
     start = end + delimiter.length;
 
-    const products = hwo.productText
+    const products = product.productText
         .slice(start)
         .split("$$")
         .map((s) => s.trim())
         .filter((s) => s !== "")
+        .filter((s) => /^[A-Z]{2}Z\d{3}/.test(s))
         .map((s) => s + "\n\n$$\n");
 
     products.forEach((value) => {
-        start = 0;
         const zone = value.slice(0, value.search(/-\d{6}-/));
         parseZone(zone);
     });
@@ -54,7 +51,7 @@ function parseZone(raw: string): void {
 }
 
 function expandZones(zoneString: string): string[] {
-    const prefix = zoneString.slice(0, 3); // e.g., "NYZ"
+    const prefix = zoneString.slice(0, 3);
     const parts = zoneString.slice(3).split("-");
     const zones: string[] = [];
 
@@ -82,4 +79,16 @@ const hwo: Product = {
     productText:
         "\n000\nFLUS41 KBGM 200106\nHWOBGM\n\nHazardous Weather Outlook\nNational Weather Service Binghamton NY\n906 PM EDT Sat Jul 19 2025\n\nNYZ023>025-044>046-055>057-062-PAZ038>040-043-044-047-048-072-210115-\nSchuyler-Chemung-Tompkins-Cortland-Chenango-Otsego-Tioga-Broome-\nDelaware-Sullivan-Bradford-Susquehanna-Northern Wayne-Wyoming-\nLackawanna-Luzerne-Pike-Southern Wayne-\n906 PM EDT Sat Jul 19 2025\n\nThis Hazardous Weather Outlook is for central New York and \nnortheast Pennsylvania.\n\n.DAY ONE...Tonight.\n\nHazardous weather is not expected at this time.\n\n.DAYS TWO THROUGH SEVEN...Sunday through Friday.\n\nScattered showers and thunderstorms are expected on Sunday. A few \nthunderstorms may turn severe and also a few locations could \nexperience excessive rainfall and localized flash flooding.\n\n.SPOTTER INFORMATION STATEMENT...\n\nWeather spotters are encouraged to report significant weather\nconditions according to Standard Operating Procedures. Please relay\nany information about severe weather to the NWS.\n\n$$\n\nNYZ009-015>018-022-036-037-210115-\nNorthern Oneida-Yates-Seneca-Southern Cayuga-Onondaga-Steuben-\nMadison-Southern Oneida-\n906 PM EDT Sat Jul 19 2025\n\nThis Hazardous Weather Outlook is for central New York.\n\n.DAY ONE...Tonight.\n\nNo hazardous weather is expected at this time.\n\n.DAYS TWO THROUGH SEVEN...Sunday through Friday.\n\nThe probability for widespread hazardous weather is low.\n\n.SPOTTER INFORMATION STATEMENT...\n\nSpotter activation is not expected at this time.\n\n$$\n",
 };
+
+const ffa: Product = {
+    id: "b8852362-b422-44c3-b006-b7fc936df9cd",
+    wmoCollectiveId: "WGUS61",
+    issuingOffice: "KBGM",
+    issuanceTime: "2025-07-17T03:53:00+00:00",
+    productCode: "FFA",
+    productName: "Flash Flood Watch",
+    productText:
+        "\n000\nWGUS61 KBGM 170353\nFFABGM\n\nFlood Watch\nNational Weather Service Binghamton NY\n1153 PM EDT Wed Jul 16 2025\n\nPAZ043-044-047-170500-\n/O.CAN.KBGM.FA.A.0010.000000T0000Z-250717T0400Z/\n/00000.0.ER.000000T0000Z.000000T0000Z.000000T0000Z.OO/\nWyoming-Lackawanna-Luzerne-\nIncluding the cities of Wilkes-Barre, Hazleton, Tunkhannock, and\nScranton\n1153 PM EDT Wed Jul 16 2025\n\n...FLOOD WATCH IS CANCELLED...\n\nThe Flash Flood Watch is cancelled for a portion of northeast \nPennsylvania, including the following areas, Lackawanna, Luzerne and \nWyoming.\n\nFlooding is no longer expected to pose a threat. Please continue to \nheed remaining road closures.\n\n$$\n\nKistner\n",
+};
 parse(hwo);
+parse(ffa);
